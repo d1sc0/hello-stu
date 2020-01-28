331af3a454532fb95662902531a763731074e81e
d1sc0 <ilovediscostu@gmail.com>
2020-01-28 10:42:04 +0000

    updateing blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,8 @@ const IndexPage = () => (
         </p>
         <p className={style.introText}>
           I'm the Product & Technology Director at{' '}
-          <a href="https://www.wearefuturegov.com/">FutureGov</a>. I work with
-          teams to design and implement services that aim to make our public
-          services more AWESOME.
+          <a href="https://www.wearefuturegov.com/">FutureGov</a>. I also run,
+          enjoy photography, love dogs and bake bread.
         </p>
         <p className={style.introText}>
           I use this site to share my personal{' '}
