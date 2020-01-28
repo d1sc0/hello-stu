f495861352f63bb366888b6c717b71d8108448a4
d1sc0 <ilovediscostu@gmail.com>
2020-01-28 07:53:55 +0000

    adding Nav into Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,22 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
 const Footer = () => {
   return (
     <footer>
       <p>
-        Built with{" "}
+        <Link to="/">Home</Link> // <Link to="/about">About</Link> //{' '}
+        <Link to="/contact">Contact</Link> // <Link to="/blog">Blog</Link>
+      </p>
+      <p>
+        Built with{' '}
         <span role="img" aria-label="heart emoji">
-          {" "}
-          ♥️{" "}
+          {' '}
+          ♥️{' '}
         </span>
         using <a href="https://www.gatsbyjs.org">Gatsby</a> | Based on a starter
-        created by <a href="https://radoslawkoziel.pl">Panr</a>
+        created by <a href="https://radoslawkoziel.pl">Panr</a> but with some
+        added <a href="https://twitter.com/_disco">@_disco</a> magic.
       </p>
     </footer>
   )
